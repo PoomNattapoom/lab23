9d7fb29f9b5c221ebbd8220a3ffa7b6ffbe7f33f
PoomNattapoom <rockyaggi@gmail.com>
2023-03-30 00:36:43 +0700

    lab23 completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
+int main(int argc, char* argv[]){
+    float x;
+    for(int i=1;i<argc;i++){
+        x+=atof(argv[i]);
+    }
+    if(argc>1){
+        cout<<"---------------------------------\n";
+        cout<<"Average of "<<argc-1<<" numbers = "<<x/(argc-1)<<"\n";
+        cout<<"---------------------------------\n";
+    }else {
+         cout<<"Please input numbers to find average.";
+    }
+    return 0;
+}
